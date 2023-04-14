37b2203c23f8df78cc20fc399ce042a08d875e63
Jarooony <24360745+jarooony@users.noreply.github.com>
2023-04-14 22:11:08 +0200

    small typo fixes, different logs

--- nulleinspeisung.py
+++ nulleinspeisung.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 
 # Diese Daten müssen angepasst werden: zeile 5 - 12
 serial = "112100000000" # Seriennummern der Hoymiles Wechselrichter
-maximum_wr = 300 # Maximum ausgabe des wechselrichters
+maximum_wr = 300 # Maximum Ausgabe des Wechselrichters
 
 dtuIP = '192.100.100.20' # IP Adresse von OpenDTU
 dtuNutzer = 'admin' # OpenDTU Nutzername
@@ -18,9 +18,9 @@ while True:
         r = requests.get(url = f'http://{dtuIP}/api/livedata/status/inverters' ).json()
 
         # Selektiert spezifische Daten aus der json response
-        reachable   = r['inverters'][0]['reachable'] # ist DTU erreichbar ?
+        reachable   = r['inverters'][0]['reachable'] # ist die DTU erreichbar ?
         producing   = int(r['inverters'][0]['producing']) # produziert der Wechselrichter etwas ?
-        altes_limit = int(r['inverters'][0]['limit_absolute']) # wo war das alte Limit gesetzt
+        altes_limit = int(r['inverters'][0]['limit_absolute']) # wie hoch war das alte Limit gesetzt
         power_dc    = r['inverters'][0]['AC']['0']['Power DC']['v']  # Lieferung DC vom Panel
         power       = r['inverters'][0]['AC']['0']['Power']['v'] # Abgabe BKW AC in Watt
 
@@ -33,7 +33,7 @@ while True:
     except:
         print("Ein Fehler ist bei der Datenverarbeitung aufgetreten")
         print("Vorhergehenden Werte werden verwendet")
-        print("Reseting.....")
+        print("Zuruecksetzen.....")
 
     # Setzt ein limit auf das Wechselrichter
     def setLimit(Serial, Limit):
@@ -41,29 +41,29 @@ while True:
         payload = f'''data={{"serial":"{Serial}", "limit_type":0, "limit_value":{Limit}}}'''
         try:
             newLimit = requests.post(url=f'http://{dtuIP}/api/limit/config', data=payload, auth=HTTPBasicAuth(dtuNutzer, dtuPasswort), headers=headers)
-            print('Konfiguration Stauts:', newLimit.json()['type'])
+            print('Konfiguration Status:', newLimit.json()['type'])
         except:
-            print("Konfiguration's fehler ... Konfiguration konnte nicht gesendet werden")
+            print("Fehlerhafte Konfig ... Konfiguration konnte nicht gesendet werden")
 
     # Werte setzen
     print("aktueller Bezug - Haus:   ",grid_sum)
     if reachable:
         # Setzen Sie den Grenzwert auf den höchsten Wert, wenn er über dem zulässigen Höchstwert liegt.
         if (altes_limit >= maximum_wr or grid_sum >= maximum_wr or setpoint >= maximum_wr):
-            print("setze Limiter auf maximum")
+            print("Setze Limiter auf Maximum")
             setpoint = maximum_wr
 
         # wir weniger bezogen als maximum_wr dann neues Limit ausrechnen
         if (grid_sum+altes_limit) <= maximum_wr:
             setpoint = grid_sum + altes_limit - 5
             print("setpoint:",grid_sum,"+",altes_limit,"-5 ")
-            print("neues Limit berechnet auf ",setpoint)
+            print("neues Limit wird gesetzt auf ",setpoint)
         if setpoint <= 100:
             setpoint = 100
-            print("setpoint: 100 minimum gesetzt")
-            print("neues Limit festgelegt auf ",setpoint)
+            print("Setpoint: 100 W Minimum gesetzt")
+            print("Neues Limit wird festgelegt auf ",setpoint)
 
-        print("setze Einspeiselimit auf: ",setpoint)
+        print("Setze Einspeiselimit auf: ",setpoint)
         # neues limit setzen
         setLimit(serial, setpoint)
         print("Solarzellenstrom:",power,"  Setpoint:",setpoint)
